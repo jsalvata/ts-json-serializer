1134a93d50ced4ea18ae35472213e5279e00c588
Christoph Bühler <buehler@users.noreply.github.com>
2017-02-14 13:29:07 +0100

    Release/1.0.0 (#4)
    * Version bump to v1.0.0
    
    * Update changelog to v1.0.0
    
    * make script executable
    
    * add description and file to prepare script
    
    * fix before deploy

--- prepare-release.js
+++ prepare-release.js
@@ -20,6 +20,13 @@ packageJson = packageJson.replace(/"version": .*,/, `"version": "${versionNumber
 
 fs.writeFileSync('./package.json', packageJson, { encoding: 'utf-8' });
 
+console.log(`Update npm-shrinkwrap.json to version v${versionNumber}`);
+
+let shrinkWrap = fs.readFileSync('./npm-shrinkwrap.json', 'utf-8');
+shrinkWrap = shrinkWrap.replace(/"version": .*,/, `"version": "${versionNumber}",`);
+
+fs.writeFileSync('./package.json', shrinkWrap, { encoding: 'utf-8' });
+
 console.log(`Update changelog`);
 
 let changelog = fs.readFileSync('./CHANGELOG.md', 'utf-8');
@@ -29,9 +36,10 @@ changelog = changelog.replace(/\[Unreleased\]\:.*v(.*)\.\.\.master/, `[Unrelease
 
 fs.writeFileSync('./CHANGELOG.md', changelog, { encoding: 'utf-8' });
 
-console.log('Commit package.json');
+console.log('Commit *.json');
 
 exec('git add package.json');
+exec('git add npm-shrinkwrap.json');
 exec(`git commit -m "Version bump to v${versionNumber}"`);
 
 console.log('Commit changelog');
